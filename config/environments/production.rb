4cc32e81886f86227e8a1b7713051ea9b2c80fc5
Tomas Hernandez <tomas.hernandez@ieee.org>
2014-12-06 12:13:45 -0800

    Implemented devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Required for Heroku
+  #Set to actual host
+  config.action_mailer.default_url_options = { host: 'https://ttoti-pinteresting.herokuapp.com/'}
+
 end
