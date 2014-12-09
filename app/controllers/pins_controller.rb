c4aa632818a5e5a65bd8702d1319d0e385ff8b1b
Tomas Hernandez <tomas.hernandez@ieee.org>
2014-12-08 21:08:54 -0800

    Added and fixed editing pins

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -47,7 +47,7 @@ class PinsController < ApplicationController
 
     def correct_user
       @pin = current_user.pins.find_by(id: params[:id])
-      redirect_to pins_path if pin.nil?, notice: "Not authorized to edit this pin"
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
